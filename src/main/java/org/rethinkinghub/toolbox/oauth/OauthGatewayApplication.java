e64dfcb141a2faa710861c3f4d336752eb45fc34
Wang <peng.wang9@hpe.com>
2016-08-04 17:01:03 +0800

    initial commit for oauth-gateway project

---
+++ src/main/java/org/rethinkinghub/toolbox/oauth/OauthGatewayApplication.java
@@ -0,0 +1,12 @@
+package org.rethinkinghub.toolbox.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthGatewayApplication.class, args);
+	}
+}
---
+++ src/test/java/org/rethinkinghub/toolbox/oauth/OauthGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package org.rethinkinghub.toolbox.oauth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OauthGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
