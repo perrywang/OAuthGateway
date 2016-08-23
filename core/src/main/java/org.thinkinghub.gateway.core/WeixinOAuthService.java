0a4aaa138614677f249c91c3b862b491e15a840e
Johnson Jiang <lizwjiang@gmail.com>
2016-08-23 13:49:44 +0800

    Refactor the whole project including creating two projects called core and gateway and change the corresponding gradle configuration file and dependencies.

--- core/src/main/java/org.thinkinghub.gateway.core/WeixinOAuthService.java
+++ core/src/main/java/org.thinkinghub.gateway.core/WeixinOAuthService.java
@@ -1,14 +1,41 @@
 package org.thinkinghub.gateway.core;
 
 import com.github.scribejava.core.builder.api.DefaultApi20;
+import com.github.scribejava.core.model.AbstractRequest;
 import com.github.scribejava.core.model.OAuthConfig;
+import com.github.scribejava.core.model.OAuthConstants;
 import com.github.scribejava.core.oauth.OAuth20Service;
+import org.thinkinghub.gateway.util.Constants;
 
 public class WeixinOAuthService extends OAuth20Service{
     public WeixinOAuthService(DefaultApi20 api, OAuthConfig config) {
         super(api, config);
     }
 
+    protected <T extends AbstractRequest> T createAccessTokenRequest(String code, T request) {
+        final OAuthConfig config = getConfig();
+        request.addParameter(Constants.WEBXIN_API_KEY, config.getApiKey());
+        request.addParameter(Constants.WEBXIN_API_SECRET, config.getApiSecret());
+        request.addParameter(OAuthConstants.CODE, code);
+        request.addParameter(OAuthConstants.REDIRECT_URI, config.getCallback());
+        final String scope = config.getScope();
+        if (scope != null) {
+            request.addParameter(OAuthConstants.SCOPE, scope);
+        }
+        request.addParameter(OAuthConstants.GRANT_TYPE, OAuthConstants.AUTHORIZATION_CODE);
+        return request;
+    }
 
+    protected <T extends AbstractRequest> T createRefreshTokenRequest(String refreshToken, T request) {
+        if (refreshToken == null || refreshToken.isEmpty()) {
+            throw new IllegalArgumentException("The refreshToken cannot be null or empty");
+        }
+        final OAuthConfig config = getConfig();
+        request.addParameter(Constants.WEBXIN_API_KEY, config.getApiKey());
+        request.addParameter(Constants.WEBXIN_API_SECRET, config.getApiSecret());
+        request.addParameter(OAuthConstants.REFRESH_TOKEN, refreshToken);
+        request.addParameter(OAuthConstants.GRANT_TYPE, OAuthConstants.REFRESH_TOKEN);
+        return request;
+    }
 
 }
