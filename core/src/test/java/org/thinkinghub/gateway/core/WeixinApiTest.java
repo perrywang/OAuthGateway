03010fc19411bd76963ae3e0c5224fee9eac75a1
Johnson Jiang <lizwjiang@gmail.com>
2016-09-07 16:39:30 +0800

    Refactor the test case for WeixinAPI

--- core/src/test/java/org/thinkinghub/gateway/core/WeixinApiTest.java
+++ core/src/test/java/org/thinkinghub/gateway/core/WeixinApiTest.java
@@ -1,16 +1,30 @@
 package org.thinkinghub.gateway.core;
 
 
+import com.github.scribejava.core.model.OAuthConfig;
 import org.junit.Test;
 import org.thinkinghub.gateway.api.WeixinApi;
 
 import static org.junit.Assert.assertEquals;
 
 public class WeixinApiTest {
-    private static final String AUTHORIZE_URL = "https://open.weixin.qq.com/connect/qrconnect?appid=%s&redirect_uri=%s&response_type=code&state=esfadsgsad34fwdef&scope=snsapi_login#wechat_redirect";
+    private final String AUTHORIZE_URL = "https://open.weixin.qq.com/connect/qrconnect";
+    private final String TOKEN_URL = "https://api.weixin.qq.com/sns/oauth2/access_token";
 
     @Test
     public void getAuthorizationBaseUrl() {
-        assertEquals(WeixinApi.instance().getAuthorizationBaseUrl(), AUTHORIZE_URL);
+        assertEquals( AUTHORIZE_URL, WeixinApi.instance().getAuthorizationBaseUrl());
     }
+
+    @Test
+    public void getAccessTokenEndpoint(){
+        assertEquals(TOKEN_URL,  WeixinApi.instance().getAccessTokenEndpoint());
+    }
+
+    @Test
+    public void getAuthorizationUrl(){
+        OAuthConfig config = new OAuthConfig("myTestAppid", "myTestSecret",null, null, null, null, null, "code",null, null,null,null);
+        assertEquals(AUTHORIZE_URL + "?response_type=code&appid=myTestAppid", WeixinApi.instance().getAuthorizationUrl(config, null));
+    }
+
 }
