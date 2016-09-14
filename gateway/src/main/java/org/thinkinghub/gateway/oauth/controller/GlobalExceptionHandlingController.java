f7039550188f7303eb045f568b0389f91b465eba
zyyettie <zhangyuyuan1@gmail.com>
2016-09-14 18:41:21 +0800

    add method to handle exception from Spring (MissingServletRequestParameterException)

--- gateway/src/main/java/org/thinkinghub/gateway/oauth/controller/GlobalExceptionHandlingController.java
+++ gateway/src/main/java/org/thinkinghub/gateway/oauth/controller/GlobalExceptionHandlingController.java
@@ -5,8 +5,11 @@ import java.sql.SQLException;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.thinkinghub.gateway.oauth.exception.GatewayException;
 
 import lombok.extern.slf4j.Slf4j;
@@ -15,10 +18,24 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class GlobalExceptionHandlingController {
 
+	@ExceptionHandler({Exception.class, RuntimeException.class})
+	@ResponseStatus(value=HttpStatus.BAD_REQUEST)
+	public String defaultError(HttpServletRequest request, Exception exception){
+		log.error("Request "+ request.getRequestURL() + " raised " + exception);
+		return "An error happened";
+	}
+	
 	@ExceptionHandler({GatewayException.class})
-	public String gatewayBusinessError(HttpServletRequest request, Exception exception){
+	public String gatewayError(HttpServletRequest request, Exception exception){
 		log.error("Request "+ request.getRequestURL() + " raised " + exception);
 		return "gatewayerror";
+	}
+	
+	@ExceptionHandler({MissingServletRequestParameterException.class})
+	@ResponseStatus(value=HttpStatus.BAD_REQUEST)
+	public String requestParamError(HttpServletRequest request, Exception exception){
+		log.error("Request "+ request.getRequestURL() + " raised " + exception);
+		return "Your request is missing parameter(s)";
 	}
 	
 	@ExceptionHandler({SQLException.class})
@@ -28,7 +45,7 @@ public class GlobalExceptionHandlingController {
 	}
 	
 	@ExceptionHandler({IOException.class})
-	public String IOError(HttpServletRequest request, Exception exception){
+	public String ioError(HttpServletRequest request, Exception exception){
 		log.error("Request "+ request.getRequestURL() + " raised " + exception);
 		return "ioerror";
 	}
